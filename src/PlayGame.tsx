2509b79a1a525e5cf64cb4d518fd583ce59fb231
Peter Brummel <pbrummel@gmail.com>
2024-09-30 16:36:58 -0500

    Created 2nd page / component for Play Game

---
+++ src/PlayGame.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const PlayGame: React.FC = () => {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold">Play Game</h1>
+      <br />
+      <Link to="/" className="btn btn-secondary">
+        Back to Home
+      </Link>
+      {/* Add more content here */}
+    </div>
+  );
+};
+
+export default PlayGame;
