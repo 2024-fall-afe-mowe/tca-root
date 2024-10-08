779b7292817e15ed74fdc2cc3632cf5b67fb7b9b
Peter Brummel <pbrummel@gmail.com>
2024-10-07 20:57:53 -0500

    Added faction dropdown selection

--- src/PlayGame.tsx
+++ src/PlayGame.tsx
@@ -8,6 +8,8 @@ export const PlayGame = () => {
     // States to store the values of both dice
     const [dice1, setDice1] = useState(0);
     const [dice2, setDice2] = useState(0);
+    const [attacker, setAttacker] = useState('');
+    const [defender, setDefender] = useState('');
 
     // Function to generate random numbers between 0 and 3 for each dice
     const rollDice = () => {
@@ -41,13 +43,48 @@ export const PlayGame = () => {
           </div>
         </div>
 
+        {/* Faction Dropdowns */}
+        <div className="dropdown-container">
+          <div className="dropdown-left">
+            <label htmlFor="attacker">Select Attacker Faction:</label>
+            <select
+              id="attacker"
+              value={attacker}
+              onChange={(e) => setAttacker(e.target.value)}
+              className="faction-select"
+            >
+              <option value="">-- Select Faction --</option>
+              <option value="Marquise de Cat">Marquise de Cat</option>
+              <option value="Eyrie Dynasties">Eyrie Dynasties</option>
+              <option value="Woodland Alliance">Woodland Alliance</option>
+              <option value="Vagabond">Vagabond</option>
+            </select>
+          </div>
+
+          <div className="dropdown-right">
+            <label htmlFor="defender">Select Defender Faction:</label>
+            <select
+              id="defender"
+              value={defender}
+              onChange={(e) => setDefender(e.target.value)}
+              className="faction-select"
+            >
+              <option value="">-- Select Faction --</option>
+              <option value="Marquise de Cat">Marquise de Cat</option>
+              <option value="Eyrie Dynasties">Eyrie Dynasties</option>
+              <option value="Woodland Alliance">Woodland Alliance</option>
+              <option value="Vagabond">Vagabond</option>
+            </select>
+          </div>
+        </div>
+
         {/* Roll Dice Button */}
         <button className="btn btn-primary mb-3" onClick={rollDice}>
           Roll Dice
         </button>
 
-        {/* Roll Dice Button */}
-        <button className = "btn btn-secondary mb-3" onClick={() => nav("/")}>
+        {/* Back to Home Button */}
+        <button className="btn btn-secondary mb-3" onClick={() => nav("/")}>
           Back to Home
         </button>
       </div>
