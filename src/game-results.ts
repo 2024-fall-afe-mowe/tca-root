cb8e626edcc0e37ffa8e1bf78a329538d8e55049
tsteelematc <tsteele@madisoncollege.edu>
2024-12-04 09:27:27 -0600

    Harvesting a game result definition and get previous players function...
    Seeing if just getting previous and new player stuff working with GameResults and API will provide a nice patter for Peter to continue with ? ? ?

--- src/game-results.ts
+++ src/game-results.ts
@@ -4,4 +4,26 @@ export interface Player {
   wins: number;
   losses: number;
   pct: string;
-}
+};
+
+export type GameResult = {
+    startTime: string;
+    endTime: string;
+    winner: string;
+    players: string[];
+};
+
+export const getPreviousPlayers = (results: GameResult[]) => {
+    
+    const previousPlayers = results.flatMap(
+        x => x.players
+    );
+
+    return(
+        [
+            ...new Set(previousPlayers)
+        ].sort(
+            (a, b) => a.localeCompare(b)
+        )
+    );
+};
